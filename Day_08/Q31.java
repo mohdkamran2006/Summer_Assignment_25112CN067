1575056b1e3821a8c57fe0db7d45d0f123043b96
Mohammad Kamran <kamransaifi972@gmail.com>
2026-06-04 20:07:32 +0530

    Add files via upload

---
+++ Day_08/Q31.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Q31 {
+    public static void main(String[] args) {
+        Scanner pth = new Scanner(System.in);
+        int a;
+
+        System.out.print("Enter number: ");
+        a = pth.nextInt();
+
+        for (int i = 1; i <= a; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print((char)(64 + j));
+            }
+            System.out.println();
+        }
+    }
+}
