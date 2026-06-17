b5da0fffe55c8b8ee7f873d4b9ffc0910a657afa
Mohammad Kamran <kamransaifi972@gmail.com>
2026-06-17 19:55:51 +0530

    Add files via upload

---
+++ Day_21/Q82.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while(str[length] != '\0')
+    {
+        length++;
+    }
+
+    printf("Reversed String: ");
+
+    for(i = length - 1; i >= 0; i--)
+    {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
