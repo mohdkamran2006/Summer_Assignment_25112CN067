a9f3c212210f097279fbe285fbf01dbfd9ce5cb4
Mohammad Kamran <kamransaifi972@gmail.com>
2026-06-27 14:01:42 +0530

    Update Q104.c

--- Day_26/Q104.c
+++ Day_26/Q104.c
@@ -2,20 +2,33 @@
 
 int main()
 {
-    FILE *fp;
-    char ch;
-    int count = 0;
+    int answer;
+    int score = 0;
 
-    fp = fopen("data.txt", "r");
+    printf("===== Quiz Application =====\n\n");
 
-    while((ch = fgetc(fp)) != EOF)
-    {
-        count++;
-    }
+    printf("Q1. Capital of India?\n");
+    printf("1. Mumbai\n2. Delhi\n3. Kolkata\n4. Chennai\n");
+    scanf("%d", &answer);
 
-    fclose(fp);
+    if(answer == 2)
+        score++;
 
-    printf("Total Characters = %d", count);
+    printf("\nQ2. C Language was developed by?\n");
+    printf("1. Dennis Ritchie\n2. James Gosling\n3. Bjarne Stroustrup\n4. Guido van Rossum\n");
+    scanf("%d", &answer);
+
+    if(answer == 1)
+        score++;
+
+    printf("\nQ3. 5 + 10 = ?\n");
+    printf("1. 12\n2. 15\n3. 20\n4. 25\n");
+    scanf("%d", &answer);
+
+    if(answer == 2)
+        score++;
+
+    printf("\nYour Score = %d/3\n", score);
 
     return 0;
 }
