8fa53a8c65ff838210048b54df2354ff11aa6d45
Mohammad Kamran <kamransaifi972@gmail.com>
2026-06-27 13:58:32 +0530

    Update Q97.c

--- Day_25/Q97.c
+++ Day_25/Q97.c
@@ -1,29 +1,49 @@
 #include <stdio.h>
 
-struct Student
-{
-    int rollNo;
-    char name[50];
-    float marks;
-};
-
 int main()
 {
-    struct Student s;
+    int a[50], b[50], c[100];
+    int n1, n2;
+    int i = 0, j = 0, k = 0;
 
-    printf("Enter Roll Number: ");
-    scanf("%d", &s.rollNo);
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
 
-    printf("Enter Name: ");
-    scanf("%s", s.name);
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n1; i++)
+    {
+        scanf("%d", &a[i]);
+    }
 
-    printf("Enter Marks: ");
-    scanf("%f", &s.marks);
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
 
-    printf("\nStudent Details\n");
-    printf("Roll Number = %d\n", s.rollNo);
-    printf("Name = %s\n", s.name);
-    printf("Marks = %.2f\n", s.marks);
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n2; i++)
+    {
+        scanf("%d", &b[i]);
+    }
+
+    i = j = k = 0;
+
+    while(i < n1 && j < n2)
+    {
+        if(a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+
+    while(i < n1)
+        c[k++] = a[i++];
+
+    while(j < n2)
+        c[k++] = b[j++];
+
+    printf("Merged Array:\n");
+
+    for(i = 0; i < k; i++)
+        printf("%d ", c[i]);
 
     return 0;
 }
