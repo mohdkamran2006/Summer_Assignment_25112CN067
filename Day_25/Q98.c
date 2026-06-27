f26df9a513c820ae1295d9542aa70ccf3f40324f
Mohammad Kamran <kamransaifi972@gmail.com>
2026-06-27 13:58:52 +0530

    Update Q98.c

--- Day_25/Q98.c
+++ Day_25/Q98.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-struct Employee
-{
-    int id;
-    char name[50];
-    float salary;
-};
-
 int main()
 {
-    struct Employee e;
+    char str1[100], str2[100];
+    int i, j;
 
-    printf("Enter Employee ID: ");
-    scanf("%d", &e.id);
+    printf("Enter first string: ");
+    scanf("%s", str1);
 
-    printf("Enter Employee Name: ");
-    scanf("%s", e.name);
+    printf("Enter second string: ");
+    scanf("%s", str2);
 
-    printf("Enter Salary: ");
-    scanf("%f", &e.salary);
+    printf("Common Characters: ");
 
-    printf("\nEmployee Details\n");
-    printf("ID = %d\n", e.id);
-    printf("Name = %s\n", e.name);
-    printf("Salary = %.2f\n", e.salary);
+    for(i = 0; str1[i] != '\0'; i++)
+    {
+        for(j = 0; str2[j] != '\0'; j++)
+        {
+            if(str1[i] == str2[j])
+            {
+                printf("%c ", str1[i]);
+                break;
+            }
+        }
+    }
 
     return 0;
 }
